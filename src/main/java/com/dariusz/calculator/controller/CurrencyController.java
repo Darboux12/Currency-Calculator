e79ac63bbad60ee64c8735415df40d9cfb55f726
Darboux12 <dariuszbula98@gmail.com>
2021-05-13 21:34:20 +0200

    Database container configuration

---
+++ src/main/java/com/dariusz/calculator/controller/CurrencyController.java
@@ -0,0 +1,11 @@
+package com.dariusz.calculator.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface CurrencyController {
+
+
+
+
+}
---
+++ src/main/java/com/dariusz/calculator/controller/dto/response/CurrencyResponse.java
@@ -0,0 +1,9 @@
+package com.dariusz.calculator.controller.dto.response;
+
+import lombok.Data;
+
+@Data
+public class CurrencyResponse {
+    private String fullName;
+    private String code;
+}

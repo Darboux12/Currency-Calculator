a0acec5c74da3afeb0690b99b785dabf3619f99c
Darboux12 <dariuszbula98@gmail.com>
2021-05-16 10:21:54 +0200

    Flyway migration bug fixed

---
+++ src/main/resources/db/migration/V2__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE calculator.Event (
+                       id VARCHAR(150) NOT NULL,
+                       date DATE,
+                       localization VARCHAR(255),
+                       description VARCHAR(255),
+                       PRIMARY KEY(id)
+);
